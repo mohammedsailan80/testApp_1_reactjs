667f5f37b30a1319c50762ae6c7cf12808361844
mohammedsailan80 <mohammedsailan80@gmail.com>
2021-10-06 13:56:42 +0300

    the Add and minus project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import AddandMinus from './pages/add_minus_form'
+function App() {
+  return (
+    <div className="App">
+      <AddandMinus />
+    </div>
+  );
+}
+
+export default App;
